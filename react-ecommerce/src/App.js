df6a2dc974d20f3d51fb67fcacdfdf79e892cef6
nabin-8 <nabinacharya740@gmail.com>
2024-04-22 17:53:56 +0545

    viewcard changes

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,9 +1,13 @@
+import MyRouters from "./MyRoutes";
+import Header from "./components/header/Header";
 import DisplayProductAPI from "./components/product/DisplayProductsAPI";
 
 function App() {
   return (
     < >
-        <DisplayProductAPI/>
+        {/* <DisplayProductAPI/> */}
+        {/* <Header/> */}
+        <MyRouters/>
     </>
   );
 }
